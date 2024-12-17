20e1c37cf678f29e77f9761101e71ebea37bbfaa
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-12-17 22:56:30 +0900

    create string helper functions

---
+++ Calculator/String Helper Functions.swift
@@ -0,0 +1,28 @@
+//
+//  String Helper Functions.swift
+//  Calculator
+//
+//  Created by joe on 12/17/24.
+//
+
+import Foundation
+
+/// Returns last character if it exists.
+/// Otherwise returns an empty string.
+func getLastChar(str: String) -> String {
+    return str.isEmpty ? "" : String(str.last!)
+}
+
+/// Returns true if last character of str is equal to char.
+func lastCharacterIsEqualTo(str: String, char: String) -> Bool {
+    let last = getLastChar(str: str)
+    return last == char
+}
+
+func formatResult(val: Double) -> String {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .decimal
+    numberFormatter.maximumFractionDigits = 16
+    return numberFormatter.string(from: NSNumber(value: val)) ?? "0"
+}
+
